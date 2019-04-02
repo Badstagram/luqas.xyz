6c9241e3848105fddb72be9a38bd1879d1eb963f
luqas <luqas.business@gmail.com>
2019-04-02 10:50:06 +0200

    04/02/2019

--- new/js/lang.js
+++ new/js/lang.js
@@ -1,12 +1,8 @@
 
 /* Languages variables */
+var lang    = language 
 var german  = de.json
 var english = en.json
 var spanish = esp.json
 var russian = rus.json
 var polnish = pl.json
-
-if (language == undefined){
-alert('This language isnt supported yet)
-
-}
