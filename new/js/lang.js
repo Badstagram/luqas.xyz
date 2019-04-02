342ce3615a035ebeff4fb0e15db8c1d3e89583f8
luqas <luqas.business@gmail.com>
2019-04-02 10:46:00 +0200

    04/02/2019

---
+++ new/js/lang.js
@@ -0,0 +1,12 @@
+
+/* Languages variables */
+var german  = de.json
+var english = en.json
+var spanish = esp.json
+var russian = rus.json
+var polnish = pl.json
+
+if (language == undefined){
+alert('This language isnt supported yet)
+
+}
