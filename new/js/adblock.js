c6b747a20324bde6365c2468cd1a341968adab2a
luqas <luqas.business@gmail.com>
2019-04-04 13:01:41 +0200

    04/04/2019

--- new/js/adblock.js
+++
@@ -1,4 +0,0 @@
-var e=document.createElement('div');
-e.id='PR44EPxgRGrsoVe';
-e.style.display='none';
-document.body.appendChild(e);
--- new/js/lang.js
+++
@@ -1,8 +0,0 @@
-
-/* Languages variables */
-var lang    = language
-var german  = de.json
-var english = en.json
-var spanish = esp.json
-var russian = rus.json
-var polnish = pl.json
