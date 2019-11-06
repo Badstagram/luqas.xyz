2d1b49f6b2afec82035b9684c0b907b440a41554
luqashimself <38897854+luqashimself@users.noreply.github.com>
2019-11-06 13:51:18 +0100

    11/06/2019

---
+++ website/new/header.php
@@ -0,0 +1,29 @@
+<html>
+ <header>
+  <nav>
+    <ul>
+     <li><a href="index">Startseite</a></li>
+     <li><a href="server">Server</a></li>
+     <li><a href="team">Team</a></li>
+     <li id="uhr" style="float:left;"></li>
+    </ul>
+  </nav>
+ </header>
+    <script lang="javascript">
+                 function uhrzeit() {
+                  var jetzt = new Date(),
+                  h = jetzt.getHours(),
+                  m = jetzt.getMinutes(),
+                  s = jetzt.getSeconds();
+                  m = fuehrendeNull(m);
+                  s = fuehrendeNull(s);
+                  document.getElementById('uhr').innerHTML = h + ':' + m + ':' + s;
+                  setTimeout(uhrzeit, 500);
+                 }
+  
+                 function fuehrendeNull(zahl) {
+                  zahl = (zahl < 10 ? '0' : '' )+ zahl;  
+                  return zahl;
+                 }
+        </script>
+</html>
---
+++ website/new/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>       
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+        <title>Startseite - luqas.xyz</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="shortcut icon" type="img/x-icon" href="./img/icons8-home-100.png"> 
+    </head>
+    <body>
+        <!-- Header -->
+        <?php
+        require "header.php";
+        ?>
+
+                
+
+        <!-- Footer -->
+        <?php
+        require "footer.php";
+        ?>
+
+    </body>
+</html>
