89bb38fb1e9f9cf0357722d667fb09989e1999cb
badstagram <52403515+badstagram@users.noreply.github.com>
2019-11-11 13:26:20 +0000

    removed bot token
    GO TO https://discordapp.com/developers/applications/ TO REGENERATE IT!!

--- discordbot/src/main/java/util/token.java
+++ discordbot/src/main/java/util/token.java
@@ -2,6 +2,6 @@ package util;
 
 public class token {
 
-    public static String TOKEN = "NjQyNjE5MjgzODQyNzkzNDgz.XcZklg.c6rY76QYI_eYe_j7-npJEF2r6Sg";
+    public static String TOKEN = "*bot token removed";
 
 }
